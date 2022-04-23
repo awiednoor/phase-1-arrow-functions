313fa44f25ae8012be31c372a5654322c46329b7
Noor Awied <nooorawied@gmail.com>
2022-04-23 20:40:40 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (a = 2000, b = 100) => a / b;
+const square = (a) => a * a;
+const add = (a, b) => a + b;
+
+console.log(divide(2000, 1000));
+console.log(square(2));
+console.log(add(3, 4));
